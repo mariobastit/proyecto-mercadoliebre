57fddb070689fad6376ee7ce3fc49f1279de8f9f
mariobastit~ <bastit.mario@gmail.com>
2022-10-25 23:03:03 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const app = express()
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log("Servidor corriendo")
+})
+
+app.use("/static", express.static(__dirname + "/public"))
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/register", (req,res) => {
+    res.sendFile(__dirname + "/views/register.html")
+})
+
+app.get("/login", (req,res) => {
+    res.sendFile(__dirname + "/views/login.html")
+})
